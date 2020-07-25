2bf60da531ee47ca48dd586f3a0fd376cb7dab8d
Benjamin Stephanus <ben.stephanus@gmail.com>
2020-07-25 20:16:29 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
-  def count_sentences
-
+  def count_sentences(sentences)
+    #binding.pry
+  sentences = self.split(".?!")
+  sentences.count
   end
+  
+  
+  
+  
 end
